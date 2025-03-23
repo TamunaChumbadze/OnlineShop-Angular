87011e00babf1dead848b21b06fbb73fd559be19
Natalia_G <nataliagabashvili@gmail.com>
2025-03-23 18:44:14 +0400

    seven commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,26 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AllProducts } from './all-products';
 import { BehaviorSubject, Subject } from 'rxjs';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  [x: string]: any;
+ 
   constructor(public http: HttpClient) { }
-
-  public gadamzidi : Subject<any> = new Subject()
-  public sxvaGadamzidi: BehaviorSubject<number> = new BehaviorSubject(0)
-
+ 
+ 
+ 
   signUp(body: any) {
     return this.http.post("https://api.everrest.educata.dev/auth/sign_up", body)
    }
-
-
+ 
+ 
   signIn(body: any) {
     return this.http.post("https://api.everrest.educata.dev/auth/sign_in", body)
   }
-
+ 
   getUser() {
     return this.http.get("https://api.everrest.educata.dev/auth",)
   }
@@ -30,24 +30,44 @@ export class ApiService {
   getProductsByBrand(brand: string, page: number = 1) {
     return this.http.get(`https://api.everrest.educata.dev/shop/products/brand/${brand}?page_index=${page}&page_size=12`);
   }
+ 
   getAllproduct(page: number = 1) {
     return this.http.get<AllProducts>(
       `https://api.everrest.educata.dev/shop/products/all?page_index=${page}&page_size=12`
     );
   }
+ 
   getBrands() {
     return this.http.get<string[]>('https://api.everrest.educata.dev/shop/products/brands');
   }
-
+ 
   postProduct(body:any) {
     return this.http.post("https://api.everrest.educata.dev/shop/cart/product", body)
   }
-
+ 
   updateProduct(body:any) {
     return this.http.patch("https://api.everrest.educata.dev/shop/cart/product", body)
   }
-
+ 
   getRatedProducts() {
     return this.http.get<any>('https://api.everrest.educata.dev/shop/products/rate');
   }
+ 
+  getProductById(id: string) {
+    return this.http.get(`https://api.everrest.educata.dev/shop/products/id/${id}`);
+  }
+ 
+  getCategories() {
+    return this.http.get<any[]>('https://api.everrest.educata.dev/shop/products/categories');
+  }
+ 
+  getProductsByCategory(categoryId: string, page: number = 1, pageSize: number = 12) {
+    return this.http.get<any>(
+      `https://api.everrest.educata.dev/shop/products/category/${categoryId}?page_index=${page}&page_size=${pageSize}`
+    );
+  }
+ 
+ 
+ 
+ 
 }
--- src/app/shop-all-products/shop-all-products.component.ts
+++ src/app/shop-all-products/shop-all-products.component.ts
@@ -4,7 +4,7 @@ import { Products } from '../products';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { BrandFilterComponent } from "../brand-filter/brand-filter.component";
-
+ 
 @Component({
   selector: 'app-shop-all-products',
   templateUrl: './shop-all-products.component.html',
@@ -18,11 +18,25 @@ export class ShopAllProductsComponent {
   public currentPage: number = 1;
   public noImages: string = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';
   public currentBrand: string = '';
-
+  public categories: any[] = [];
+ 
+ 
   constructor(private service: ApiService, private router: Router, private cookies: CookieService) {
     this.showAllProducts();
+    this.loadCategories();
   }
-
+  loadCategories() {
+    this.service.getCategories().subscribe((data: any) => {
+      this.categories = data;
+    });
+  }
+ 
+  onCategorySelected(categoryId: string) {
+    this.service.getProductsByCategory(categoryId).subscribe((data: any) => {
+      this.productList = data.products;
+      this.filteredProductList = this.productList;
+    });
+  }
   showAllProducts(page: number = 1, brand: string = '') {
     this.currentPage = page;
     if (brand === 'all') {
@@ -48,28 +62,48 @@ export class ShopAllProductsComponent {
       });
     }
   }
-
+ 
   goToPage(page: number) {
     this.showAllProducts(page, this.currentBrand);
   }
-
-  addToCart(item: any) {
-    if (this.cookies.get('user')) {
-      let info = {
-        id: item._id,
-        quantity: 1,
-      };
-      this.service.getUser().subscribe((data: any) =>
-        data.cartID
-          ? this.service.updateProduct(info).subscribe((data: any) => console.log(data))
-          : this.service.postProduct(info).subscribe((data: any) => console.log(data))
-      );
-    } else {
-      alert('You are not authorized!');
-      this.router.navigate(['/login']);
-    }
+ 
+ addToCart(item: any) {
+  const userCookie = this.cookies.get('user');
+ 
+  if (userCookie) {
+    let info = {
+      id: item._id,
+      quantity: 1,
+    };
+ 
+    this.service.getUser().subscribe(
+      (data: any) => {
+        if (data && data.cartID) {
+          this.service.updateProduct(info).subscribe(
+            (response: any) => console.log('Product updated:', response),
+            (error) => console.error('Update error:', error)
+          );
+        } else {
+          this.service.postProduct(info).subscribe(
+            (response: any) => console.log('Product added:', response),
+            (error) => console.error('Add error:', error)
+          );
+        }
+      },
+      (error) => {
+        console.error('User fetch error:', error);
+        alert('Authorization check failed. Please log in again.');
+        this.router.navigate(['/login']);
+      }
+    );
+  } else {
+    console.log("User is not authorized");
+    alert('You are not authorized!');
+    this.router.navigate(['/login']);
   }
-
+}
+ 
+ 
   onBrandSelected(brand: string) {
     this.currentBrand = brand;
     this.showAllProducts(1, brand);
